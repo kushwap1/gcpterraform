d8846d15e1f95ff8acd28c73510d838514aa8ba0
kushwap1 <pankaj.kushwaha@yahoo.com>
2021-01-31 13:00:34 +0000

    net

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ provider "google" {
 
   credentials = file("key.json")
 
-  project = "playground-s-11-65999d7d"
+  project = var.project
   region  = "us-central1"
   zone    = "us-central1-c"
 }
@@ -20,13 +20,30 @@ resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
 
+resource "google_compute_firewall" "default" {
+  name    = "pankaj-firewall"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "8080", "1000-2000"]
+  }
+
+  source_tags = ["web","ssh"]
+}
+
 resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
+  count        = "2"
+  name         = "terraform-instance-${count.index + 1}"
   machine_type = "f1-micro"
 
-  provisioner "local-exec" {
-    command = "echo ${google_compute_instance.vm_instance.name}:  ${google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip} >> ip_address.txt"
-  }
+#  provisioner "local-exec" {
+#    command = "echo ${google_compute_instance.vm_instance.name}:  ${google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip} >> ip_address.txt"
+#  }
 
   boot_disk {
     initialize_params {
@@ -38,6 +55,61 @@ resource "google_compute_instance" "vm_instance" {
     network = google_compute_network.vpc_network.name
     access_config {
     }
+  }
+}
+
+resource "google_storage_bucket" "bucket" {
+  name          = "pankaj0811-gcloud-bucket"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "Delete"
+    }
+  }
+}
+
+resource "google_sql_database" "database" {
+  name     = "my-database"
+  instance = google_sql_database_instance.instance.name
+}
+
+resource "google_sql_database_instance" "instance" {
+  name   = "my-database-instance"
+  region = "us-central1"
+  settings {
+    tier = "db-f1-micro"
+  }
+
+}
+
+resource "random_id" "db_name_suffix" {
+  byte_length = 4
+}
+
+resource "google_sql_database_instance" "master" {
+  name = "master-instance-${random_id.db_name_suffix.hex}"
+
+  settings {
+    tier = "db-f1-micro"
+  }
+}
+
+resource "google_sql_user" "users" {
+  name     = "me"
+  instance = google_sql_database_instance.master.name
+  host     = "me.com"
+  password = "changeme"
+}
+
+resource "google_monitoring_notification_channel" "basic" {
+  display_name = "Test Notification Channel"
+  type         = "email"
+  labels = {
+    email_address = "pankaj.kushwaha@yahoo.com"
   }
 }
 
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "project" {
+  type = string
+  default = "playground-s-11-5e9a8375"
+  description = "This is default project"
+}
